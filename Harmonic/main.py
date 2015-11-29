13f0fccdb1d4ca13cec122a0fb02e083184c8bee
CristhianMotoche <cristhian.motoche@gmail.com>
2015-11-29 10:18:05 -0500

    Control limites harmonic

--- Harmonic/main.py
+++ Harmonic/main.py
@@ -2,6 +2,7 @@
 from harmonic import *
 from utilidades import *
 import threading
+import math
 
 def colectarDigitos(d, n, m):
     global allDigits
@@ -16,10 +17,17 @@ def main():
     n = int(input("Ingrese N (1 <= N <= 10^8): \n>>"))
     threads = 2
 
-    t1 = threading.Thread(target=colectarDigitos, args=(d,n,n//2))
+    if n%2 == 0:
+        max = n//2
+        min = n//2 - 1
+    else:
+        max = math.ceil(n/2)
+        min = math.floor(n/2)
+
+    t1 = threading.Thread(target=colectarDigitos, args=(d,n,max))
     t1.start()
 
-    t2 = threading.Thread(target=colectarDigitos, args=(d,n//2 - 1,1))
+    t2 = threading.Thread(target=colectarDigitos, args=(d,min,1))
     t2.start()
 
     t1.join()
@@ -31,4 +39,3 @@ def main():
 
 if __name__=="__main__":
     main()
-
